f8e404a4644cf59a85332167e3f861824f93947e
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-03 19:41:50 +0900

    Create Example090302.java

---
+++ Example090302.java
@@ -0,0 +1,48 @@
+class Example090302{
+
+	int [] arr = new int [6];
+
+	public void method01(){
+
+		for(int i=0;i<arr.length;i++){
+			arr[i]=new int();
+		}
+
+		int x = (Math.random()*45)+1;
+		int y = (Math.random()*45)+1;
+
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr.length;j++){
+				if(x != y){
+
+					
+				}
+			}
+			
+		}
+	}
+
+	public void method02(){
+
+		
+		
+
+	}
+
+
+	public static void main(String[] args){
+
+
+
+		/**로또번호 6개를 배열방에서 중복되지 않게 만들어서 출력하기
+		 ( 1~ 45까지)  출력을 할때 내림차순으로 출력하게 한다.
+
+		   조건(메소드 4개) : 
+		   1)중복체크하는 메소드(배열방에 넣기 전에 체크)
+           2)숫자 6개를 배열방에 저장하는 메소드
+           3)배열방의 값을 내림차순으로 정렬하는 메소드
+           4)배열의 값을 화면에 출력하는 메소드*/
+
+		
+	}
+}
